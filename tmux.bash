96b98ee5f738203182bd590352f22c0b1b950bf0
kaoru.inoue <kaoru.inoue@classi.jp>
2024-02-14 15:01:01 +0000

    シェルを起動する方法を変更

---
+++ tmux.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PATH=$PATH:~/.rd/bin
+
+~/.rd/bin/nerdctl \
+  run \
+    --privileged \
+    --network host \
+    --volume $HOME:$HOME \
+    --user 0:0 \
+    -it \
+      cli-tool-docker sh -c \
+        "hostname $(hostname) && \
+        useradd \
+          --uid $(id -u) \
+          --gid $(id -g) \
+	        --home $HOME \
+	        --shell /usr/bin/fish \
+	        $USER 2> /dev/null && \
+        echo '${USER} ALL=(ALL:ALL) NOPASSWD: ALL' > /etc/sudoers.d/$USER && \
+	      su - $USER -c tmux"
