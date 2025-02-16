8961d9331118871c4049f3e10ee9755a362e0398
kaoru.inoue <winds.from.oz@gmail.com>
2025-02-16 17:50:17 +0900

    use gosu

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@
 USER_ID=${UID:-503}
 GROUP_ID=${GID:-1000}
 USER_NAME=${USER_NAME:-customuser}
+HOME_DIR=$HOME
 
 # グループの作成
 if ! getent group ${GROUP_ID} >/dev/null; then
@@ -12,15 +13,8 @@ fi
 
 # ユーザーの作成
 if ! id -u ${USER_ID} >/dev/null 2>&1; then
-    useradd -m -s /bin/bash -u ${USER_ID} -g ${GROUP_ID} ${USER_NAME}
+    useradd -m -s /bin/bash -u ${USER_ID} -g ${GROUP_ID} -d ${HOME_DIR} ${USER_NAME}
 fi
 
-# ユーザーのホームディレクトリを取得
-# USER_HOME=$(getent passwd ${USER_NAME} | cut -d: -f6)
-
-# ホームディレクトリの所有者を設定
-# chown -R ${USER_ID}:${GROUP_ID} ${USER_HOME}
-
 # 指定したユーザーとしてコマンドを実行
-# exec gosu ${USER_NAME} "$@"
-exec "$@"
+exec gosu ${USER_NAME} "$@"
