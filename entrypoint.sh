0f37dc18bc0e71377c7221d86938f75bc0b016d4
kaoru.inoue <winds.from.oz@gmail.com>
2025-04-17 23:04:45 +0900

    fix-aqua-dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,6 +16,14 @@ if ! id -u ${USER_ID} >/dev/null 2>&1; then
     useradd -M -s /bin/bash -u ${USER_ID} -g ${GROUP_ID} -d ${HOME_DIR} ${USER_NAME}
 fi
 
+# /opt/aqua ディレクトリの存在確認と作成
+if [ ! -d /opt/aqua ]; then
+    mkdir -p /opt/aqua
+fi
+
+# Aqua ディレクトリの所有権を変更
+chown -R ${USER_ID}:${GROUP_ID} /opt/aqua
+
 # 指定したユーザーとしてコマンドを実行
 # 引数が無ければ tail -F /dev/null を実行
 if [ $# -eq 0 ]; then
