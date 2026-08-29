4922bb1cb9c226403a39bcb0b36c71e760741531
kaoru.inoue <winds.from.oz@gmail.com>
2026-08-29 13:48:04 +0900

    環境変数の引き渡す候補を増やす

--- shell.bash
+++ shell.bash
@@ -220,8 +220,12 @@ TERMINAL="${TERM:-screen-256color-bce}"
 # --init-groups が /etc/group を元に supplementary groups を設定します（= dialout を有効にしシリアルコンソールにアクセスできるようにする）
 # exec --user uid:gid で入ると supplementary groups が初期化されない
 # root で exec → su / setpriv --init-groups で降りると、initgroups(3) が走って解決
+_TERM_PROGRAM="$(tmux show-environment TERM_PROGRAM 2>/dev/null | sed 's/TERM_PROGRAM=//')"
+_TERM_PROGRAM="${_TERM_PROGRAM:-${TERM_PROGRAM}}"
+_TERM_PROGRAM_VERSION="$(tmux show-environment TERM_PROGRAM_VERSION 2>/dev/null | sed 's/TERM_PROGRAM_VERSION=//')"
+_TERM_PROGRAM_VERSION="${_TERM_PROGRAM_VERSION:-${TERM_PROGRAM_VERSION}}"
 $CONTAINER_CMD exec -it \
-  --env TERM="${TERMINAL}" --env TERM_PROGRAM="${TERM_PROGRAM}" --env TERM_PROGRAM_VERSION="${TERM_PROGRAM_VERSION}" --env LC_ALL="${LOCALE_LC_ALL}" --env TZ="${TIMEZONE}" \
+  --env TERM="${TERMINAL}" --env TERM_PROGRAM="${_TERM_PROGRAM}" --env TERM_PROGRAM_VERSION="${_TERM_PROGRAM_VERSION}" --env LC_ALL="${LOCALE_LC_ALL}" --env TZ="${TIMEZONE}" \
   --user 0:0 \
   "$CONTAINER_ID" bash -lc \
   'exec setpriv --reuid='"$(id -u)"' --regid='"$(id -g)"' --init-groups '"$SHELL_CMD"' "$@"' bash "$@"
